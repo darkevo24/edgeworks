27ed3615f6f9d627b9caed0e1dcf31589ec9403d
darkevo2424 <timotius.albert@luxpmsoft.com>
2022-12-07 07:49:46 +0700

    done

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, Image, TouchableOpacity, View,ScrollView } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [data,setData] = useState([]);
+  const [list,setList] = useState([]);
+  const url = "https://auntieanne-demo.proseller.io/product/api/productpreset/loadcategory/webOrdering/64e4e47b-b428-4ee3-97f5-62bfa03c8ba6/";
+  const urlItem = "https://auntieanne-demo.proseller.io/product/api/productpreset/loaditems/webOrdering/64e4e47b-b428-4ee3-97f5-62bfa03c8ba6/"
+  useEffect(function(){
+    axios.post(url).then(function(res){
+      setData(res.data.data);
+      console.log(res.data.data);
+    })
+  },[])
+
+  function Press(id){
+    console.log("id : ",id);
+    axios.post(urlItem + id).then(function(res){
+      console.log("item-id " , res.data);
+      setList(res.data.data);
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.parent}>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        nestedScrollEnabled = {true}
+        contentContainerStyle={styles.ScrollView} >
+      {data.map(function(item){
+        return (
+          <View style={styles.item} key={item.id}>
+            <TouchableOpacity onPress={() => Press(item.id)}>
+            <Text>{item.name}</Text>
+            </TouchableOpacity>
+          </View>
+        )
+      })}
+      </ScrollView>
+      </View>
+      <View>
+        {list.map(function(item){
+          console.log(item.product.defaultImageURL);
+          return (
+            <View style={styles.list} key={item.id}>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text>{item.product.description}</Text>
+              <Image source={{ uri:item.product.defaultImageURL,width:100,height:100}} />
+            </View>
+          )
+        })}
+      </View>
     </View>
   );
 }
@@ -14,7 +61,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  ScrollView : {
+    display : "flex",
+    flexDirection : "row",
+    justifyContent : 'flex-start',
+    alignItems : 'flex-start',
+    flexGrow : 1
+  },
+  item : {
+    margin : 50,
+  },
+  list : {
+    display : "flex",
+    justifyContent : "center",
+    alignItems : 'center',
+    margin : 20
+  },
+  name : {
+    fontWeight:"bold"
+  }
 });
